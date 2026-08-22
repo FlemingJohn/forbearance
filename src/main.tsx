dd3dc42a62c386c5d4d87d3c1b2fc97702ac5c3d
FlemingJohn <flemjohn08@gmail.com>
2026-08-22 22:36:17 +0530

    feat: mount the app and load global styles

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import "./styles/tokens.css";
+import "./styles/base.css";
+import "./styles/utilities.css";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container is missing from index.html");
+}
+
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
