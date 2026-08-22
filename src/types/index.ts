73588fbd237857bf038d721403dfb9ea2e4d0c47
FlemingJohn <flemjohn08@gmail.com>
2026-08-22 22:18:53 +0530

    feat: re-export types from a single entry point

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type { EvidenceGrade, Finding, FindingDescription } from "./finding";
+export type { CaseFile, Exhibit, ExhibitRole } from "./caseFile";
+export type { Market, RegistryTotals } from "./market";
+export type {
+  AttentionShift,
+  DocketCandidate,
+  ExaminerDecision,
+  ExaminerState,
+} from "./docket";
+export type { ChainStatus, TourPanel, TourStep } from "./tour";
