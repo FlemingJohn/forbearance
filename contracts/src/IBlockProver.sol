26438ba57e3280384fe8ea3fa657f6a5b8acff2e
FlemingJohn <flemjohn08@gmail.com>
2026-08-23 13:09:42 +0530

    feat: add the block prover interface

---
+++ contracts/src/IBlockProver.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+interface IBlockProver {
+    struct MerkleProofEntry {
+        bytes32 hash;
+        bool isLeft;
+    }
+
+    struct MerkleProof {
+        bytes32 root;
+        MerkleProofEntry[] siblings;
+    }
+
+    struct ContinuityProof {
+        bytes32 lowerEndpointDigest;
+        bytes32[] roots;
+    }
+
+    function verifyAndEmit(
+        uint64 chainKey,
+        uint64[] calldata heights,
+        bytes[] calldata encodedTransactions,
+        MerkleProof[] calldata merkleProofs,
+        ContinuityProof calldata sharedContinuityProof
+    ) external returns (bool);
+
+    function calculateTxIndex(MerkleProof calldata merkleProof)
+        external
+        view
+        returns (uint64);
+}
